db0c621058cae3e0ecf7b6967771ac20f4670f34
Hao Tran <haovn577@gmail.com>
2019-02-12 21:51:42 +0700

    The very first time.

---
+++ src/client/setup/routes.js
@@ -0,0 +1,36 @@
+// Imports
+import React from 'react'
+
+// App Imports
+import Home from '../components/home/Home'
+import About from '../components/home/About'
+import BlogsContainer from '../components/blog/BlogsContainer'
+import BlogContainer from '../components/blog/BlogContainer'
+import BlogAdd from '../components/blog/BlogAdd'
+
+// Routes
+const routes = [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/blogs',
+        component: BlogsContainer
+    },
+    {
+        path: '/blog/:id',
+        component: BlogContainer
+    },
+    {
+        path: '/blog-add',
+        component: BlogAdd
+    }
+]
+
+export default routes
